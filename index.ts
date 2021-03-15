57af53b5cc80b21a47f9bc454c14f561c8181618
Strikeeaglechase <Strikeeaglechase@gmail.com>
2021-03-15 13:51:57 -0400

    Updated ports

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
+// Server ports: 	10112
+// 					10113
 import express from "express";
 import { WebsocketServer } from "./WebsocketServer.js";
 import { App } from "./app.js";
 const app = express();
-const port = 8080;
+const port = 10112;
 
 app.use(express.static('public'))
 app.listen(port, () => {
 	console.log(`Server open on port ${port}`);
 });
 
-const wss = new WebsocketServer(8090);
+const wss = new WebsocketServer(10113);
 const golApp = new App(wss);
 golApp.init();
